97ed8851d77bc3b6b1cd32041e660dc4751fde3c
Martin Vega <mvega@longomatch.com>
2024-01-10 15:20:25 +0100

    Add PoC code

---
+++ Model.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class DataBaseContext : DbContext
+{
+    // Terminal commands
+    // dotnet tool install --global dotnet-ef
+    // dotnet add package Microsoft.EntityFrameworkCore.Design
+    // dotnet ef migrations add InitialCreate
+    // dotnet ef database update
+
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<Player> Players { get; set; }
+
+    public string DbPath { get; }
+
+    public DataBaseContext()
+    {
+        var folder = Environment.SpecialFolder.Desktop;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "teams.db");
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Team>().Property(t => t.Name).IsRequired();
+        modelBuilder.Entity<Team>().Property(t => t.Country).IsRequired(false);
+        modelBuilder.Entity<Team>()
+            .HasMany(t => t.Players)
+            .WithOne(p => p.Team)
+            .HasForeignKey(p => p.TeamId)
+            .IsRequired();
+
+        modelBuilder.Entity<Player>().Property(p => p.Name).IsRequired();
+        modelBuilder.Entity<Player>().Property(p => p.Position).IsRequired(false);
+    }    
+}
+
+public class Team
+{
+    // Primary key
+    public int Id { get; set; }
+
+    // Mandatory field
+    public string Name { get; set; } = null!;
+
+    // Optional field
+    public string? Country { get; set; }
+
+    // Collection Navigation
+    public List<Player> Players { get; } = new List<Player>();
+}
+
+public class Player
+{
+    public int Id { get; set; }
+
+    // Mandatory field
+    public string Name { get; set; } = null!;
+
+    // Optional field
+    public string? Position { get; set; }
+
+    // Required foreign key
+    public int TeamId { get; set; }
+
+    // Required reference navigation to principal
+    public Team Team { get; set; } = null!;
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        try
+        {
+            using (var db = new DataBaseContext())
+            {
+                await db.Database.MigrateAsync();
+                await db.Database.EnsureCreatedAsync();
+
+                var isInitializingData = true;
+                if (isInitializingData)
+                {
+                    var bcn = new Team()
+                    {
+                        Name = "FC Barcelona",
+                        Country = "Spain",
+                    };
+                    var pbcn1 = new Player()
+                    {
+                        Name = "Marc Andre Ter Stegen",
+                        Position = "GK",
+                    };
+                    bcn.Players.Add(pbcn1);
+                    await db.AddAsync(bcn);
+                    await db.SaveChangesAsync();
+
+                    var rm = new Team()
+                    {
+                        Name = "Real Madrid CF",
+                        Country = "Spain",
+                    };
+                    await db.AddAsync(rm);
+                    await db.SaveChangesAsync();
+                }
+                else
+                {
+                    foreach (var team in db.Teams.Include(t => t.Players))
+                    {
+                        System.Diagnostics.Debug.WriteLine($"Team - Id: {team.Id} Name: {team.Name} Country: {team.Country} Players total: {team.Players.Count}");
+                        foreach (var player in team.Players)
+                        {
+                            System.Diagnostics.Debug.WriteLine($"Player - Id: {player.Id} Name: {player.Name} Position: {player.Position} TeamId: {player.TeamId} Team: {player.Team.Name}");
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+        }
+
+        Console.ReadLine();
+    }
+}
